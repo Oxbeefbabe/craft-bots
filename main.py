d2659b419803c04106ab4c616e5249b67a3c9a8c
LNemiro <ludwignemiro@yahoo.com>
2021-12-14 15:47:18 +0000

    This branch is for the Rules Based Agent (RBA)

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from agents import test_agent
+from agents import basic_rba
 from craftbots import craft_bots
 
 if __name__ == '__main__':
-    craft_bots.start_simulation(agent_class=test_agent.TestAgent,
+    craft_bots.start_simulation(agent_class=basic_rba.Basic_RBA,
                                 use_gui=True,
                                 modifier_file="craftbots/initialisation_files/simple_modifiers",
                                 world_modifier_file="craftbots/initialisation_files/simple_world_gen_modifiers",
