579b49da16d17c1572069f637b25916e5074ec40
LNemiro <ludwignemiro@yahoo.com>
2021-12-26 15:58:01 +0000

    Outputting total time for the simulation to run

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import time
+
 from agents import basic_rba
+
 from craftbots import craft_bots
 
 if __name__ == '__main__':
+    start = time.perf_counter()
     craft_bots.start_simulation(agent_class=basic_rba.Basic_RBA,
                                 use_gui=True,
                                 modifier_file="craftbots/initialisation_files/simple_modifiers",
                                 world_modifier_file="craftbots/initialisation_files/simple_world_gen_modifiers",
                                 rule_file="craftbots/initialisation_files/simple_rules"
                                 )
+    print(f"\n{time.perf_counter() - start}")
