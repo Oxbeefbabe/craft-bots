3b6e9b3bc008491986c66d1e20b6851c5af7afb8
LNemiro <ludwignemiro@yahoo.com>
2022-03-08 13:43:28 +0000

    Bash script to run multiple instances of the evaluator with different rules

---
+++ run_eval.bash
@@ -0,0 +1,8 @@
+python evaluator.py 3 TAA RBA simple small &
+python evaluator.py 3 TAA RBA simple small &
+python evaluator.py 3 TAA RBA simple large &
+python evaluator.py 3 TAA RBA simple large &
+python evaluator.py 3 TAA RBA complex small &
+python evaluator.py 3 TAA RBA complex small &
+python evaluator.py 3 TAA RBA complex large &
+python evaluator.py 3 TAA RBA complex large
