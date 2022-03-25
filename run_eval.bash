8c2eb9932efe9240de56317c626eab658d3d3414
LNemiro <ludwignemiro@yahoo.com>
2022-03-25 15:15:12 +0000

    Updating rules and eval script

--- run_eval.bash
+++ run_eval.bash
@@ -9,7 +9,7 @@ python3 evaluator.py 50 TAA RBA complex large
 python3 evaluator.py 25 TAA complex small &
 python3 evaluator.py 25 TAA complex small &
 python3 evaluator.py 25 TAA complex small &
-python3 evaluator.py 25 TAA complex small
+python3 evaluator.py 25 TAA complex small &
 python3 evaluator.py 50 TAA RBA simple large &
 python3 evaluator.py 50 TAA RBA simple large &
 python3 evaluator.py 50 TAA RBA simple large &
