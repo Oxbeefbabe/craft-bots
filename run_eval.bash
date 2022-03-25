5176445d1a4ddc5cb65ba7154fdf93a884fcb319
LNemiro <ludwignemiro@yahoo.com>
2022-03-25 16:12:02 +0000

    Updating rules and eval script

--- run_eval.bash
+++ run_eval.bash
@@ -1,23 +1,19 @@
-python3 evaluator.py 50 TAA RBA simple large &
-python3 evaluator.py 50 TAA RBA simple large &
-python3 evaluator.py 50 TAA RBA simple large &
-python3 evaluator.py 50 TAA RBA simple large &
-python3 evaluator.py 50 TAA RBA complex large &
-python3 evaluator.py 50 TAA RBA complex large &
-python3 evaluator.py 50 TAA RBA complex large &
-python3 evaluator.py 50 TAA RBA complex large
+python3 evaluator.py 25 TAA RBA simple large &
+python3 evaluator.py 25 TAA RBA simple large &
+python3 evaluator.py 25 TAA RBA simple large &
+python3 evaluator.py 25 TAA RBA simple large &
+python3 evaluator.py 15 TAA RBA complex large &
+python3 evaluator.py 15 TAA RBA complex large &
+python3 evaluator.py 15 TAA RBA complex large &
+python3 evaluator.py 15 TAA RBA complex large &
+python3 evaluator.py 15 TAA RBA complex large &
+python3 evaluator.py 15 TAA RBA complex large &
+python3 evaluator.py 15 TAA RBA complex large &
+python3 evaluator.py 15 TAA RBA complex large &
 python3 evaluator.py 25 TAA complex small &
 python3 evaluator.py 25 TAA complex small &
 python3 evaluator.py 25 TAA complex small &
 python3 evaluator.py 25 TAA complex small &
-python3 evaluator.py 50 TAA RBA simple large &
-python3 evaluator.py 50 TAA RBA simple large &
-python3 evaluator.py 50 TAA RBA simple large &
-python3 evaluator.py 50 TAA RBA simple large &
-python3 evaluator.py 50 TAA RBA complex large &
-python3 evaluator.py 50 TAA RBA complex large &
-python3 evaluator.py 50 TAA RBA complex large &
-python3 evaluator.py 50 TAA RBA complex large
 python3 evaluator.py 25 TAA complex small &
 python3 evaluator.py 25 TAA complex small &
 python3 evaluator.py 25 TAA complex small &
