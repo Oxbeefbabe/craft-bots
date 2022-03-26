370170464e2dbd7c8bc1d6ee0b3786ad4448a21f
LNemiro <ludwignemiro@yahoo.com>
2022-03-26 17:12:12 +0000

    Updating rules and eval script

--- run_eval.bash
+++ run_eval.bash
@@ -1,20 +1,20 @@
-python3 evaluator.py 25 TAA RBA simple large &
-python3 evaluator.py 25 TAA RBA simple large &
-python3 evaluator.py 25 TAA RBA simple large &
-python3 evaluator.py 25 TAA RBA simple large &
-python3 evaluator.py 15 TAA RBA complex large &
-python3 evaluator.py 15 TAA RBA complex large &
-python3 evaluator.py 15 TAA RBA complex large &
-python3 evaluator.py 15 TAA RBA complex large &
-python3 evaluator.py 15 TAA RBA complex large &
-python3 evaluator.py 15 TAA RBA complex large &
-python3 evaluator.py 15 TAA RBA complex large &
-python3 evaluator.py 15 TAA RBA complex large &
-python3 evaluator.py 25 TAA complex small &
-python3 evaluator.py 25 TAA complex small &
-python3 evaluator.py 25 TAA complex small &
-python3 evaluator.py 25 TAA complex small &
-python3 evaluator.py 25 TAA complex small &
-python3 evaluator.py 25 TAA complex small &
-python3 evaluator.py 25 TAA complex small &
-python3 evaluator.py 25 TAA complex small
+python3 evaluator.py 15 TAA RBA simple large &
+python3 evaluator.py 15 TAA RBA simple large &
+python3 evaluator.py 15 TAA RBA simple large &
+python3 evaluator.py 15 TAA RBA simple large &
+python3 evaluator.py 3 TAA RBA complex large &
+python3 evaluator.py 3 TAA RBA complex large &
+python3 evaluator.py 3 TAA RBA complex large &
+python3 evaluator.py 3 TAA RBA complex large &
+python3 evaluator.py 3 TAA RBA complex large &
+python3 evaluator.py 3 TAA RBA complex large &
+python3 evaluator.py 3 TAA RBA complex large &
+python3 evaluator.py 3 TAA RBA complex large &
+python3 evaluator.py 7 TAA complex small &
+python3 evaluator.py 7 TAA complex small &
+python3 evaluator.py 7 TAA complex small &
+python3 evaluator.py 7 TAA complex small &
+python3 evaluator.py 7 TAA complex small &
+python3 evaluator.py 7 TAA complex small &
+python3 evaluator.py 7 TAA complex small &
+python3 evaluator.py 7 TAA complex small
